5f63c22d13987cf04b3e76de80b816a0ffa306bd
Sebastian <turndriverside@gmail.com>
2025-08-08 13:20:40 -0700

    Add auto_updater and set APP_VERSION 1.0.0

---
+++ src/auto_updater.py
@@ -0,0 +1,4 @@
+APP_VERSION = "1.0.0"
+
+UPDATE_CHECK_URL = "https://api.github.com/repos/sebastiank112/ai-image-renamer/releases/latest"
+DOWNLOAD_URL_TEMPLATE = "https://github.com/sebastiank112/ai-image-renamer/releases/download/v{version}/AI_Image_Renamer_Mac_v{version}.zip"
